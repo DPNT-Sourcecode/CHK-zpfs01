50fbcae3887ad14fbfe69ede0633e80b816a55ee
sbx_user1051 <sbx_user1051@169.254.13.170>
2024-12-18 16:20:38 +0000

    Wed Dec 18 16:20:38 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -20,6 +20,8 @@ def checkout(skus):
     for item in shopping_cart:
         if item in multi_offers_dict:
             for offer in multi_offers_dict[item]:
+                print(shopping_cart[item])
+                print(offer)
                 if shopping_cart[item] > offer:
                     total += (math.floor(shopping_cart[item] / offer) * multi_offers_dict[item][offer])
                     remainder = shopping_cart[item] % offer
@@ -27,13 +29,11 @@ def checkout(skus):
 
                     break
         
-        print(item)
         if item in one_free_offers_dict:
             free_item = one_free_offers_dict[item][1]
             offer_item_num = shopping_cart[item]
 
             while offer_item_num >= one_free_offers_dict[item][0] and shopping_cart[free_item]:
-                print('decreasing')
                 shopping_cart[free_item] -= 1
                 offer_item_num -= one_free_offers_dict[item][0]
 
@@ -47,6 +47,6 @@ def checkout(skus):
     return total
 
 
-price = checkout('AAABBEEE')
+price = checkout('AAAAA')
 print(price) 
 
